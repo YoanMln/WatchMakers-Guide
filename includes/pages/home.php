ec00aca1bba464fadc590faef2eaa8e8e8c311ae
YoanMln <yoan.moulin@hotmail.fr>
2025-06-26 21:19:58 +0200

    delete  h1

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -1,8 +1,9 @@
 <header>
     <div class="header-container">
-        <img class="header-img" src="assets/images/img/home-img.webp" alt="illustration page acceuil du site representant un mécanisme d'horlogerie">
-        <h1>Calibre1200</h1>
-        <p>Explorez la précision et la passion de l’horlogerie à travers un regard technique et authentique</p>
-        <a href="index.php?pages=Decouverte">Découverte</a>
+        
+        <h2>Explorez la précision et la passion de l’horlogerie à travers un regard technique et authentique</h2>
+        <div class="btn-container-home">
+            <a href="index.php?pages=Decouverte">Découverte</a>
+        </div>
     </div>
 </header>
