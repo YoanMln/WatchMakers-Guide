d94e9b743dca8805e780a86a008702c73ee3e079
YoanMln <yoan.moulin@hotmail.fr>
2025-06-26 21:19:33 +0200

    ajout div container-title

--- includes/pages/Decouverte.php
+++ includes/pages/Decouverte.php
@@ -1,137 +1,137 @@
-<main>
+<div class="container-title">
     <div class="title-decouverte">
         <h1>L'histoire de l'horlogerie</h1>
         <h2>Découvrir les moments clés ...</h2>
-</main>
-<article>
     </div>
-    <div class="container">
-        <div class="timeline">
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">1665</span></h4>
-                    <h5 class="timeline-intro">Naissance de Daniel Jean-Richard, "Bressel"</h5>
-                    <p>Horloger jurassien, il est la figure tutélaire de l’horlogerie suisse traditionnelle, symbole de l’établissage.</p>
+    <article>
+</div>
+<div class="container">
+    <div class="timeline">
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">1665</span></h4>
+                <h5 class="timeline-intro">Naissance de Daniel Jean-Richard, "Bressel"</h5>
+                <p>Horloger jurassien, il est la figure tutélaire de l’horlogerie suisse traditionnelle, symbole de l’établissage.</p>
 
-                </div>
-            </div>
-
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">1681</span></h4>
-                    <h5 class="timeline-intro">Première montre suisse dévoilée</h5>
-                    <p>Daniel Jean-Richard présente la première montre suisse, inspirée d’un modèle anglais.</p>
-
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">1700-1750</span>Début XVIIIème siècle</h4>
-                    <h5 class="timeline-intro">Migration des huguenots et transfert de savoir-faire</h5>
-                    <p>Les protestants français fuient la persécution et apportent les innovations anglaises et hollandaises, notamment le balancier et l’échappement à ancre</p>
-
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">Fin-1700</span>Fin XVIIIème siècle</h4>
-                    <h5 class="timeline-intro">Essor massif de l’horlogerie suisse</h5>
-                    <p>Genève emploie plus de 20,000 personnes, la production suisse dépasse 130,000 pièces par an.</p>
-
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">1850</span> Révolution industrielle américaine</h4>
-                    <h5 class="timeline-intro"></h5>
-                    <p>American Waltham Watch Company introduit la production industrielle avec pièces interchangeables, challengeant l’établissage suisse.</p>
-
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">1876</span>Exposition universelle de Philadelphie</h4>
-                    <h5 class="timeline-intro"></h5>
-                    <p>Jacques David exhorte l’industrie suisse à moderniser ses méthodes pour rester compétitive.</p>
-
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">1829</span>Grande Dépression et regroupement</h4>
-                    <h5 class="timeline-intro"></h5>
-                    <p>Crise mondiale provoque la naissance des grands groupes SSIH (Omega, Tissot) et ASUAG (Longines, Mido, Hamilton).</p>
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">1970-1980</span>Crise du Quartz</h4>
-                    <h5 class="timeline-intro"></h5>
-                    <p>Les montres à quartz japonaises déferlent, réduisant la part de marché suisse de 50% à 15% en 8 ans.</p>
-
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">1983</span>Fusion et création du Swatch Group</h4>
-                    <h5 class="timeline-intro"></h5>
-                    <p>SSIH et ASUAG fusionnent sous Nicolas Hayek, donnant naissance à la SMH (devenue Swatch Group) qui sauve l’industrie avec la Swatch.</p>
-
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">2000s</span>Renouveau de l'horlogerie mécanique</h4>
-                    <h5 class="timeline-intro"></h5>
-                    <p>Retour en force des montres mécaniques et automatiques, tandis que le quartz se cantonne aux modèles d’entrée de gamme.</p>
-                </div>
-            </div>
-            <div class="timeline-container primary">
-                <div class="timeline-icon">
-                    <i class="far fa-grin-wink"></i>
-                </div>
-                <div class="timeline-body">
-                    <h4 class="timeline-title"><span class="badge">2020s</span>Défis contemporains</h4>
-                    <h5 class="timeline-intro"></h5>
-                    <p>Montres connectées, impact environnemental, influence digitale : l’horlogerie suisse continue de se réinventer.</p>
-                </div>
             </div>
         </div>
 
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">1681</span></h4>
+                <h5 class="timeline-intro">Première montre suisse dévoilée</h5>
+                <p>Daniel Jean-Richard présente la première montre suisse, inspirée d’un modèle anglais.</p>
 
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">1700-1750</span>Début XVIIIème siècle</h4>
+                <h5 class="timeline-intro">Migration des huguenots et transfert de savoir-faire</h5>
+                <p>Les protestants français fuient la persécution et apportent les innovations anglaises et hollandaises, notamment le balancier et l’échappement à ancre</p>
 
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">Fin-1700</span>Fin XVIIIème siècle</h4>
+                <h5 class="timeline-intro">Essor massif de l’horlogerie suisse</h5>
+                <p>Genève emploie plus de 20,000 personnes, la production suisse dépasse 130,000 pièces par an.</p>
 
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">1850</span> Révolution industrielle américaine</h4>
+                <h5 class="timeline-intro"></h5>
+                <p>American Waltham Watch Company introduit la production industrielle avec pièces interchangeables, challengeant l’établissage suisse.</p>
+
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">1876</span>Exposition universelle de Philadelphie</h4>
+                <h5 class="timeline-intro"></h5>
+                <p>Jacques David exhorte l’industrie suisse à moderniser ses méthodes pour rester compétitive.</p>
+
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">1829</span>Grande Dépression et regroupement</h4>
+                <h5 class="timeline-intro"></h5>
+                <p>Crise mondiale provoque la naissance des grands groupes SSIH (Omega, Tissot) et ASUAG (Longines, Mido, Hamilton).</p>
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">1970-1980</span>Crise du Quartz</h4>
+                <h5 class="timeline-intro"></h5>
+                <p>Les montres à quartz japonaises déferlent, réduisant la part de marché suisse de 50% à 15% en 8 ans.</p>
+
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">1983</span>Fusion et création du Swatch Group</h4>
+                <h5 class="timeline-intro"></h5>
+                <p>SSIH et ASUAG fusionnent sous Nicolas Hayek, donnant naissance à la SMH (devenue Swatch Group) qui sauve l’industrie avec la Swatch.</p>
+
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">2000s</span>Renouveau de l'horlogerie mécanique</h4>
+                <h5 class="timeline-intro"></h5>
+                <p>Retour en force des montres mécaniques et automatiques, tandis que le quartz se cantonne aux modèles d’entrée de gamme.</p>
+            </div>
+        </div>
+        <div class="timeline-container primary">
+            <div class="timeline-icon">
+                <i class="far fa-grin-wink"></i>
+            </div>
+            <div class="timeline-body">
+                <h4 class="timeline-title"><span class="badge">2020s</span>Défis contemporains</h4>
+                <h5 class="timeline-intro"></h5>
+                <p>Montres connectées, impact environnemental, influence digitale : l’horlogerie suisse continue de se réinventer.</p>
+            </div>
+        </div>
     </div>
-    </div>
+
+
+
+
+</div>
+</div>
 
 </article>
