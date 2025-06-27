b996817555fffb15649ee32964348b59fad97cef
YoanMln <yoan.moulin@hotmail.fr>
2025-06-27 20:57:26 +0200

    Corrections

--- includes/pages/MesureDuTemps.php
+++ includes/pages/MesureDuTemps.php
@@ -3,21 +3,21 @@
 
     <div id="timeline-1" class="timeline-container">
         <div class="timeline-header">
-            <h1 class="timeline-header__title">Histoire de la meusure du temps</h1>
+            <h1 class="timeline-header__title">Histoire de la mesure du temps</h1>
             <h3 class="timeline-header__subtitle">Des premières civilisations au monde moderne,<br> la mesure du temps a été essentielle pour organiser la vie sociale, religieuse et économique. <br> D’abord basée sur l’observation des astres, elle s’est peu à peu perfectionnée grâce à l’invention de moyens toujours plus précis <br> pour se repérer dans le temps.</h3>
         </div>
 
         <div class="timeline">
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Gnonom.webp" alt="gnomon ancien" />
-                    <h2 class="timeline__content-title">Le gnomon <h2>
-                            <p class="timeline__content-desc">IIIème millénaire avt J-C</p>
-                            <p class="timeline__content-desc">C'est le plus ancien instrument à mesurer le temps. Il est formé d'un style vertical qui peut être aussi un tronc d'arbre en colonne une aiguille de pierre de l'ombre projeté sur la terre sert à la détermination du temps. Nombre la plus courte indique le vrai les différentes directions de l'ombre permettent de diviser la journée en période égale</p>
+                    <h2 class="timeline__content-title">Le gnomon </h2>
+                    <p class="timeline__content-desc">IIIème millénaire avt J-C</p>
+                    <p class="timeline__content-desc">C'est le plus ancien instrument à mesurer le temps. Il est formé d'un style vertical qui peut être aussi un tronc d'arbre en colonne une aiguille de pierre de l'ombre projeté sur la terre sert à la détermination du temps. Nombre la plus courte indique le vrai les différentes directions de l'ombre permettent de diviser la journée en période égale</p>
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Cadran-solaire.webp" alt="cadran solaire" />
                     <h2 class="timeline__content-title">Le cadran solaire</h2>
@@ -26,7 +26,7 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Clepsydre.webp" alt="Clépsydre" />
                     <h2 class="timeline__content-title">La clépsydre</h2>
@@ -35,16 +35,16 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Bougie.webp" alt="bougie graduée" />
-                    <h2 class="timeline__content-title">La bougie</h2>
+                    <h2 class="timeline__content-title">La bougie graduée</h2>
                     <p class="timeline__content-desc">Vers 870</p>
                     <p class="timeline__content-desc"> la bougie ou la chandelle pouvait servir à une double utilisation, celle d'éclairer, mais aussi en étant graduée, elle pouvait mesurer le temps</p>
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Foliot.webp" alt="horloge à Foliot" />
                     <h2 class="timeline__content-title">Horloges à Foliot</h2>
@@ -53,7 +53,7 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/ressort-barillet.webp" alt=" ressort de barillet" />
                     <h2 class="timeline__content-title">Le ressort de barillet</h2>
@@ -62,7 +62,7 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/galilée.webp" alt="galilée" />
                     <h2 class="timeline__content-title">Le pendule</h2>
@@ -71,7 +71,7 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/spiral-huygens.webp" alt="ressort spiral Huygens" />
                     <h2 class="timeline__content-title">Le ressort spiral</h2>
@@ -80,7 +80,7 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/paliers.webp" alt="palies en pierre" />
                     <h2 class="timeline__content-title">Paliers en pierre</h2>
@@ -89,7 +89,7 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/longitude.webp" alt="longitude" />
                     <h2 class="timeline__content-title">Calcul de la longitude</h2>
@@ -98,7 +98,7 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/compensateur.webp" alt="pendule compensateur" />
                     <h2 class="timeline__content-title">Le pendule compensateur</h2>
@@ -107,7 +107,7 @@
                 </div>
             </div>
 
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/echappement.webp" alt="échappement à détente" />
                     <h2 class="timeline__content-title">Échappement à détente</h2>
@@ -115,14 +115,14 @@
                     <p class="timeline__content-desc">Pierre Le Roy applique le premier échappement à détente</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/calvin.webp" alt="arrivée de l'horlogerie en suisse" />
                     <h2 class="timeline__content-title">Arrivée de l'horlogerie en Suisse</h2>
                     <p class="timeline__content-desc">Après la révocation de l’édit de Nantes en 1685, de nombreux huguenots se réfugient à Genève, apportant avec eux leur savoir-faire, notamment en horlogerie. Dans une cité marquée par la rigueur calviniste, les bijoutiers en déclin se reconvertissent en fabriquant des boîtes de montre. C’est ainsi que naît l’horlogerie suisse.</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/marine.webp" alt="horloge marine" />
                     <h2 class="timeline__content-title">Chronomètre de marine</h2>
@@ -130,7 +130,7 @@
                     <p class="timeline__content-desc">Ferdinand Berthoud, mécanicien de la marine construit des montres marines. </p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/invention-echappement.webp" alt="invention échappement" />
                     <h2 class="timeline__content-title">L'échappement</h2>
@@ -138,7 +138,7 @@
                     <p class="timeline__content-desc">George Grahan invente le mécanisme à encre encore utilisé sur de nombreuses horloges du XIXème siècle.</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/ancre-suisse.webp" alt="échappement ancre Suisse" />
                     <h2 class="timeline__content-title">Séparation de l'ancre et du pendule</h2>
@@ -146,7 +146,7 @@
                     <p class="timeline__content-desc">Thomas Mudge sépare l'ancre du pendule pour créer l'ancre libre. Le mécanisme est très intéressant pour les oscillateurs à balanciers circulaires. C'est de cette invention que sera issu l'échappement à ancre Suisse.</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/suisse.webp" alt="patchwork manufacture d'horlogerie Suisse" />
                     <h2 class="timeline__content-title">Essor de l'horlogerie Suisse</h2>
@@ -154,7 +154,7 @@
                     <p class="timeline__content-desc">Apparitions des marques suivantes : Longine fondée en 1832, Jaeger-LeCoultre fondée en 1839, Patek Philippe fondée en 1839, Ulysse Nardin fondée en 1846, Omega fondée en 1848, Tissot fondée en 1853, Zenith fondée en 1865, Piaget fondée en 1874, Audemars Piguet fondée en 1875.</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/rubis.webp" alt="processus Verneuil , création rubis synthétiques" />
                     <h2 class="timeline__content-title">Découverte du processus Verneuil</h2>
@@ -162,7 +162,7 @@
                     <p class="timeline__content-desc"> Auguste Verneuil découvrit le processus de fusion à la flamme de l'oxyde d'alumine aujourd'hui communément appelé processus Verneuil, qui reste de nos jours d'actualité pour obtenir des corindon et des rubis synthétiques.</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/hans.webp" alt="Hans Wilsdorf" />
                     <h2 class="timeline__content-title">Montre poignet</h2>
@@ -170,7 +170,7 @@
                     <p class="timeline__content-desc">Peu à peu la montre se porte au poignet et c'est Hans Wilsdorf, fondateur de Rolex qui démocratique cette mode.</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/quartz.webp" alt="montre quartz" />
                     <h2 class="timeline__content-title">oscillateurs à Quartz</h2>
@@ -178,7 +178,7 @@
                     <p class="timeline__content-desc"> Les oscillateurs et les horloges à quartz sont introduits</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/harwood.webp" alt="pub première montre braclet par John Harwood" />
                     <h2 class="timeline__content-title">Première montre bracelet</h2>
@@ -186,7 +186,7 @@
                     <p class="timeline__content-desc"> la première montre bracelet à remontage automatique fut développée par l'Anglais John Harwood</p>
                 </div>
             </div>
-            <div class="timeline-item">
+            <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/mercedes.webp" alt="Pub premère montre étanche est née , Rolex Oyster" />
                     <h2 class="timeline__content-title">Traversée de la Manche</h2>
