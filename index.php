6432d1ab915caa367f3a28d89328576289891f1e
YoanMln <yoan.moulin@hotmail.fr>
2025-06-27 12:39:43 +0200

    Modification front controller

--- index.php
+++ index.php
@@ -3,11 +3,15 @@ $pages = $_GET['pages'] ?? 'home';
 
 include 'includes/header.php';
 
-$path = "includes/pages/$pages.php";
-if (file_exists($path)) {
-    include $path;
+
+$allowed_pages = array_map(function ($file) {
+    return basename($file, '.php');
+}, glob('includes/pages/*.php'));
+
+if (in_array($pages, $allowed_pages)) {
+    include "includes/pages/$pages.php";
 } else {
-    echo "<h2> Page introuvable</h2>";
+    echo "<h2>Page introuvable</h2>";
 }
 
 include 'includes/footer.php';
