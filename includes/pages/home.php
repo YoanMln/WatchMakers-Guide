d81314433ca4740ee2427ec4742f790a77d825a6
YoanMln <yoan.moulin@hotmail.fr>
2025-06-27 21:26:46 +0200

    Ajout container h2

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -3,7 +3,9 @@
         <div class="title-h1">
             <h1>The Watchmaker's Guide </h1>
         </div>
-        <h2>Explorez la précision et la passion de l’horlogerie à travers un regard technique et authentique</h2>
+        <div class="intro-container">
+            <h2>Explorez la précision et la passion de l’horlogerie à travers un regard technique et authentique</h2>
+        </div>
         <div class="btn-container-home">
             <a href="index.php?pages=MesureDuTemps">Histoire de la mesure du temps</a>
         </div>
