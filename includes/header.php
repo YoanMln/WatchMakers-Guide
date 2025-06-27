4436097b4aa80a96fc04c44ef7013618d6020f05
YoanMln <yoan.moulin@hotmail.fr>
2025-06-27 21:54:56 +0200

    Ajout favicon + meta seo

--- includes/header.php
+++ includes/header.php
@@ -3,11 +3,15 @@
 
 <head>
     <meta charset="UTF-8">
+    <title><?= htmlspecialchars($seo['title']) ?></title>
+    <meta name="description" content="<?= htmlspecialchars($seo['description']) ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Watchmaker's Guide </title>
+    <link rel="icon" href="assets/images/logo/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/style.css">
     <?php if (file_exists("assets/css/{$pages}.css")) : ?>
         <link rel="stylesheet" href="assets/css/<?= $pages ?>.css">
+
     <?php endif; ?>
 </head>
 
