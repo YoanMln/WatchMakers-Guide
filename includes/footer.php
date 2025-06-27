2951be572ec795baaa6a2749b1d75bb70f891d9e
YoanMln <yoan.moulin@hotmail.fr>
2025-06-27 17:07:59 +0200

    Ajout footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,13 @@
+<footer class="footer">
+    <div class="footer__content">
+        <p class="footer__brand">The Watchmaker's Guide</p>
+        <p class="footer__text">&copy; 2025 – Tous droits réservés</p>
+
+    </div>
+</footer>
+
 <script src="script/burger.js"></script>
 </body>
+
 
 </html>
