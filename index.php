5ec331b8af69ddf040d6f661baf8bf2caa16731d
YoanMln <yoan.moulin@hotmail.fr>
2025-06-27 12:45:29 +0200

    Front controller switch

--- index.php
+++ index.php
@@ -3,15 +3,22 @@ $pages = $_GET['pages'] ?? 'home';
 
 include 'includes/header.php';
 
-
-$allowed_pages = array_map(function ($file) {
-    return basename($file, '.php');
-}, glob('includes/pages/*.php'));
-
-if (in_array($pages, $allowed_pages)) {
-    include "includes/pages/$pages.php";
-} else {
-    echo "<h2>Page introuvable</h2>";
+switch ($pages) {
+    case 'home':
+        include 'includes/pages/home.php';
+        break;
+    case 'Outils-de-lhorloger':
+        include 'includes/pages/Outils-de-lhorloger.php';
+        break;
+    case 'Techniques':
+        include 'includes/pages/Techniques.php';
+        break;
+    case 'Lexique':
+        include 'includes/pages/Lexique.php';
+        break;
+    default:
+        echo "<h2>Page introuvable</h2>";
+        break;
 }
 
 include 'includes/footer.php';
