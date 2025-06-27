f7c64ec07a399bd88749687af4617bc3dde56699
YoanMln <yoan.moulin@hotmail.fr>
2025-06-27 21:54:32 +0200

    Ajout fichier SEO

---
+++ includes/SEOHelper.php
@@ -0,0 +1,27 @@
+<?php
+function getSeoData($pages)
+{
+    $seoData = [
+        'home' => [
+            'title' => 'Accueil - À la découverte du temps | The Watchmakers Guide',
+            'description' => 'Explorez l’univers fascinant de l’horlogerie, de la mesure du temps aux techniques d’assemblage.',
+        ],
+        'MesureDuTemps' => [
+            'title' => 'Mesure du temps - Frise historique | The Watchmakers Guide',
+            'description' => 'Découvrez les grandes étapes de la mesure du temps à travers une frise chronologique interactive.',
+        ],
+        'Techniques' => [
+            'title' => 'Techniques horlogères - Posage et Emboîtage',
+            'description' => 'Plongez dans les gestes techniques de l’horlogerie avec des visuels détaillés du posage et de l’emboîtage.',
+        ],
+        'Outils-de-lhorloger' => [
+            'title' => 'Outils de l’horloger - Les indispensables',
+            'description' => 'Apprenez à reconnaître les outils essentiels utilisés par les horlogers professionnels.',
+        ],
+    ];
+
+    return $seoData[$pages] ?? [
+        'title' => 'The Watchmakers Guide',
+        'description' => 'Le site pour explorer la passion du temps, des techniques et des outils horlogers.',
+    ];
+}
