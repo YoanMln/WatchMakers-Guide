3f36c554de99662b27e73642debf8494b4ce56c2
YoanMln <yoan.moulin@hotmail.fr>
2025-07-07 20:57:44 +0200

    Modification footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="footer__content">
-        <p class="footer__brand">Watchmaker's Guide</p>
-        <p class="footer__text">&copy; -2025– Tous droits réservés</p>
+        <p class="footer__name">Watchmaker's Guide</p>
+        <p class="footer__text">&copy; 2025 – Réalisé par <a href="https://yoanmoulin.dev" target="_blank" target="_blank" rel="noopener noreferrer">Yoan Mln</a> - Tous droits réservés.
 
     </div>
 </footer>
