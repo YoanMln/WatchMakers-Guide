7e1669a2ef8b71213f1d3d53f32b421f309f3a55
YoanMln <yoan.moulin@hotmail.fr>
2025-07-12 21:24:16 +0200

    Modifications class

--- includes/pages/MesureDuTemps.php
+++ includes/pages/MesureDuTemps.php
@@ -12,7 +12,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Gnonom.webp" alt="gnomon ancien" />
                     <h2 class="timeline__content-title">Le gnomon </h2>
-                    <h3 class="timeline__content-desc">IIIème millénaire av J.-C</h3>
+                    <h3 class="timeline__content-date">IIIème millénaire av J.-C</h3>
                     <p class="timeline__content-desc">C'est le plus ancien instrument à mesurer le temps. Il est formé d'un style vertical qui peut être aussi : un tronc d'arbre, en colonne, une aiguille de pierre, dont l'ombre projetée sur la terre sert à la détermination du temps. l'ombre la plus courte indique le midi vrai. Les différentes directions de l'ombre permettent de diviser la journée en périodes égales</p>
                 </div>
             </div>
@@ -21,7 +21,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Cadran-solaire.webp" alt="cadran solaire" />
                     <h2 class="timeline__content-title">Le cadran solaire</h2>
-                    <h3 class="timeline__content-desc">1500 av J.-C</h3>
+                    <h3 class="timeline__content-date">1500 av J.-C</h3>
                     <p class="timeline__content-desc"> Instrument de même nature que le gnomon, mais plus perfectionné, il peut être horizontal ou vertical. Le style est parallèle à l'axe de rotation de la terre.</p>
                 </div>
             </div>
@@ -30,7 +30,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Clepsydre.webp" alt="Clépsydre" />
                     <h2 class="timeline__content-title">La clépsydre</h2>
-                    <h3 class="timeline__content-desc">Partie d'Egypte en 1500 av J.-C, la clépsydre s’est ensuite répandue chez les Romains en 159 av J.-C </h3>
+                    <h3 class="timeline__content-date">Partie d'Egypte en 1500 av J.-C, la clépsydre s’est ensuite répandue chez les Romains en 159 av J.-C </h3>
                     <p class="timeline__content-desc"> Garde-temps constitué d'un récipient rempli d'eau avec un trou à sa partie inférieure. À mesure que l'eau s'écoule par le trou, son niveau dans le récipient baisse. Ce niveau indique les différentes heures de la journée.</p>
                 </div>
             </div>
@@ -39,7 +39,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Bougie.webp" alt="bougie graduée" />
                     <h2 class="timeline__content-title">La bougie graduée</h2>
-                    <h3 class="timeline__content-desc">Vers 870</h3>
+                    <h3 class="timeline__content-date">Vers 870</h3>
                     <p class="timeline__content-desc"> La bougie ou chandelle horaire, pouvait servir à une double utilisation, celle d'éclairer, mais aussi en étant graduée, elle pouvait mesurer le temps</p>
                 </div>
             </div>
@@ -48,7 +48,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/Foliot.webp" alt="horloge à Foliot" />
                     <h2 class="timeline__content-title">Horloges à Foliot</h2>
-                    <h3 class="timeline__content-desc">1370</h3>
+                    <h3 class="timeline__content-date">1370</h3>
                     <p class="timeline__content-desc"> La véritable horloge mécanique apparaît. Un poids suspendu à une corde fournit de l'énergie à la machine, tandis qu'un système de régulation, appelé verge et foliot, interrompt régulièrement la chute du poids. N'étant pas très précise, elle possède qu'une aiguille, celle des heures</p>
                 </div>
             </div>
@@ -57,16 +57,16 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/ressort-barillet.webp" alt=" ressort de barillet" />
                     <h2 class="timeline__content-title">Le ressort de barillet</h2>
-                    <h3 class="timeline__content-desc">1430</h3>
+                    <h3 class="timeline__content-date">1430</h3>
                     <p class="timeline__content-desc"> Le ressort, logé dans un barillet fut appliqué aux horloges vers 1430, la montre avec moteur fait son apparition vers 1500</p>
                 </div>
             </div>
 
             <div class="timeline-item" data-text="Histoire de la mesure du temps">
                 <div class="timeline__content">
-                    <img class="timeline__img" src="assets/images/mesure-du-temps/galilée.webp" alt="galilée" />
-                    <h2 class="timeline__content-title">Le pendule</h2>
-                    <h3 class="timeline__content-desc">XVIème siècle</h3>
+                    <img class="timeline__img" src="assets/images/mesure-du-temps/pendule-galilee.webp" alt="galilée" />
+                    <h2 class="timeline__content-title">Le pendule de Galilée</h2>
+                    <h3 class="timeline__content-date">XVIème siècle</h3>
                     <p class="timeline__content-desc"> Galilée aurait découvert les propriétés du pendule en observant le lustre de la cathédrale de Pise qui Oscillait.</p>
                 </div>
             </div>
@@ -75,7 +75,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/spiral-huygens.webp" alt="ressort spiral Huygens" />
                     <h2 class="timeline__content-title">Le ressort spiral</h2>
-                    <h3 class="timeline__content-desc">1675</h3>
+                    <h3 class="timeline__content-date">1675</h3>
                     <p class="timeline__content-desc"> Moins de 20 ans après l'invention du pendule, Christiaan Huygens fait une autre grande découverte pour l'horlogerie. En 1675, il invente le ressort spirale.</p>
                 </div>
             </div>
@@ -84,7 +84,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/paliers.webp" alt="palies en pierre" />
                     <h2 class="timeline__content-title">Paliers en pierre</h2>
-                    <h3 class="timeline__content-desc">1700</h3>
+                    <h3 class="timeline__content-date">1700</h3>
                     <p class="timeline__content-desc"> Vers 1700 en Angleterre, utilisation de pierre percée servant de coussinets de pivotement pour les balancer</p>
                 </div>
             </div>
@@ -93,7 +93,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/longitude.webp" alt="longitude" />
                     <h2 class="timeline__content-title">Calcul de la longitude</h2>
-                    <h3 class="timeline__content-desc">1714</h3>
+                    <h3 class="timeline__content-date">1714</h3>
                     <p class="timeline__content-desc">Avec d'autres États, le parlement anglais met au concours "toutes méthodes capable de déterminer la longitude en mer" avec une prime de 10 000 livres pour un résultat n'excédant pas 1° d'erreur, 15 000 livres pour 40 min et 20 000 livres pour 1/2° ou moins</p>
                 </div>
             </div>
@@ -102,7 +102,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/compensateur.webp" alt="pendule compensateur" />
                     <h2 class="timeline__content-title">Le pendule compensateur</h2>
-                    <h3 class="timeline__content-desc">1725</h3>
+                    <h3 class="timeline__content-date">1725</h3>
                     <p class="timeline__content-desc">Inventé pour corriger les effets de la dilatation thermique sur la longueur du pendule, qui influence la précision des horloges. Il est composé de deux métaux aux coefficients de dilatation différents, de manière à ce que leurs expansions se compensent. Ce type de pendule est notamment associé à John Harrison, qui a aussi développé d’autres dispositifs pour améliorer la précision des horloges marines.</p>
                 </div>
             </div>
@@ -111,7 +111,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/echappement.webp" alt="échappement à détente" />
                     <h2 class="timeline__content-title">Échappement à détente</h2>
-                    <h3 class="timeline__content-desc">1748/1754</h3>
+                    <h3 class="timeline__content-date">1748/1754</h3>
                     <p class="timeline__content-desc">Pierre Le Roy applique le premier échappement à détente</p>
                 </div>
             </div>
@@ -126,7 +126,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/marine.webp" alt="horloge marine" />
                     <h2 class="timeline__content-title">Chronomètre de marine</h2>
-                    <h3 class="timeline__content-desc">1767/1769</h3>
+                    <h3 class="timeline__content-date">1767/1769</h3>
                     <p class="timeline__content-desc">Ferdinand Berthoud, mécanicien de la marine construit des montres marines. </p>
                 </div>
             </div>
@@ -134,7 +134,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/invention-echappement.webp" alt="invention échappement" />
                     <h2 class="timeline__content-title">L'échappement</h2>
-                    <h3 class="timeline__content-desc">XVIIIème siècle</h3>
+                    <h3 class="timeline__content-date">XVIIIème siècle</h3>
                     <p class="timeline__content-desc">George Grahan invente le mécanisme à encre encore utilisé sur de nombreuses horloges du XIXème siècle.</p>
                 </div>
             </div>
@@ -142,7 +142,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/ancre-suisse.webp" alt="échappement ancre Suisse" />
                     <h2 class="timeline__content-title">Séparation de l'ancre et du pendule</h2>
-                    <h3 class="timeline__content-desc">XVIIIème siècle</h3>
+                    <h3 class="timeline__content-date">XVIIIème siècle</h3>
                     <p class="timeline__content-desc">Thomas Mudge sépare l'ancre du pendule pour créer l'ancre libre. Le mécanisme est très intéressant pour les oscillateurs à balanciers circulaires. C'est de cette invention que sera issu l'échappement à ancre Suisse.</p>
                 </div>
             </div>
@@ -150,7 +150,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/suisse.webp" alt="patchwork manufacture d'horlogerie Suisse" />
                     <h2 class="timeline__content-title">Essor de l'horlogerie Suisse</h2>
-                    <h3 class="timeline__content-desc">XIXème siècle</h3>
+                    <h3 class="timeline__content-date">XIXème siècle</h3>
                     <p class="timeline__content-desc">Apparitions des marques suivantes : Longine fondée en 1832, Jaeger-LeCoultre fondée en 1839, Patek Philippe fondée en 1839, Ulysse Nardin fondée en 1846, Omega fondée en 1848, Tissot fondée en 1853, Zenith fondée en 1865, Piaget fondée en 1874, Audemars Piguet fondée en 1875.</p>
                 </div>
             </div>
@@ -158,7 +158,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/rubis.webp" alt="processus Verneuil , création rubis synthétiques" />
                     <h2 class="timeline__content-title">Découverte du processus Verneuil</h2>
-                    <h3 class="timeline__content-desc">1902</h3>
+                    <h3 class="timeline__content-date">1902</h3>
                     <p class="timeline__content-desc"> Auguste Verneuil découvrit le processus de fusion à la flamme de l'oxyde d'alumine aujourd'hui communément appelé processus Verneuil, qui reste de nos jours d'actualité pour obtenir des corindon et des rubis synthétiques.</p>
                 </div>
             </div>
@@ -166,7 +166,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/hans.webp" alt="Hans Wilsdorf" />
                     <h2 class="timeline__content-title">Montre poignet</h2>
-                    <h3 class="timeline__content-desc">Fin XIXème siècle début XXème siècle</h3>
+                    <h3 class="timeline__content-date">Fin XIXème siècle début XXème siècle</h3>
                     <p class="timeline__content-desc">Peu à peu la montre se porte au poignet et c'est Hans Wilsdorf, fondateur de Rolex qui démocratise cette mode.</p>
                 </div>
             </div>
@@ -174,7 +174,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/quartz.webp" alt="montre quartz" />
                     <h2 class="timeline__content-title">Oscillateurs à Quartz</h2>
-                    <h3 class="timeline__content-desc">1920</h3>
+                    <h3 class="timeline__content-date">1920</h3>
                     <p class="timeline__content-desc"> Les oscillateurs et les horloges à quartz sont introduits</p>
                 </div>
             </div>
@@ -182,7 +182,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/harwood.webp" alt="pub première montre braclet par John Harwood" />
                     <h2 class="timeline__content-title">Première montre bracelet</h2>
-                    <h3 class="timeline__content-desc">1923</h3>
+                    <h3 class="timeline__content-date">1923</h3>
                     <p class="timeline__content-desc">La première montre bracelet à remontage automatique fut développée par l'Anglais John Harwood</p>
                 </div>
             </div>
@@ -190,7 +190,7 @@
                 <div class="timeline__content">
                     <img class="timeline__img" src="assets/images/mesure-du-temps/mercedes.webp" alt="Pub premère montre étanche est née , Rolex Oyster" />
                     <h2 class="timeline__content-title">Traversée de la Manche</h2>
-                    <h3 class="timeline__content-desc">1927</h3>
+                    <h3 class="timeline__content-date">1927</h3>
                     <p class="timeline__content-desc"> Mercedes Gleitze traverse la Manche à la nage avec à son poignet, une Rolex Oyster, la montre étanche est née.</p>
                 </div>
             </div>
