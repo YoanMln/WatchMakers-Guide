e24cb8ae8abc425b6f9329559e96fb6d17bf44db
YoanMln <yoan.moulin@hotmail.fr>
2025-08-20 12:11:05 +0200

    Structure du quiz

---
+++ includes/pages/Quiz.php
@@ -0,0 +1,12 @@
+<header>
+    <h1>Quiz interactif</h1>
+
+    <div class="container">
+        <div id="difficulty"></div>
+        <div id="progression"></div>
+        <div id="question"></div>
+        <div id="answers"></div>
+        <p id="results"></p>
+        <p id="retry"></p>
+    </div>
+</header>
