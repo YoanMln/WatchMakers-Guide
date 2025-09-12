adb9d42fffb41db28df4ffe093fb2ad952a64c81
YoanMln <yoan.moulin@hotmail.fr>
2025-09-12 11:32:10 +0200

    Header copyright

--- includes/pages/Quiz.php
+++ includes/pages/Quiz.php
@@ -1,3 +1,20 @@
+<?php
+/*
+ * WatchMakers Guide - Site éducatif horlogerie
+ * Copyright (C) 2025 Yoan Moulin
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ */
+?>
+
 <main>
     <div class="header-container header-container-quiz">
         <h1 class="title-quiz">Quiz interactif</h1>
